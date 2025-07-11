213a30b93b2563547476539b57c288b3715f2821
OfflineBot <flintstonelg@gmail.com>
2025-07-11 12:49:46 +0200

    starting full conv

--- src/ai/ai.jl
+++ src/ai/ai.jl
@@ -2,3 +2,4 @@
 include("./fully_connected.jl")
 include("./basic_conv.jl")
 include("./basic_conv_bias.jl")
+include("./full_conv.jl")
--- src/cnn_modules.jl
+++ src/cnn_modules.jl
@@ -3,11 +3,12 @@ module cnn_modules
 
 include("./conv/basic_conv/basic_conv.jl")
 include("./conv/basic_conv_bias/basic_conv_bias.jl")
+include("./conv/full_conv/full_conv.jl")
+
 include("./fully_connected/fully_connected.jl")
 include("./utils/utils.jl")
 
 include("./ai/ai.jl")
 
-basic_conv_bias()
 
 end
